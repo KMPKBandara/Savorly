a1e08fc797db33528760c54553a6c08d7810eb76
Pramuditha <pramudithakavinda01@gmail.com>
2025-03-06 21:50:24 +0530

    Set the API

--- savorty/src/components/Search.jsx
+++ savorty/src/components/Search.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from "react";
 
-const Search = () => {
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "cb2f64834c80463daafb44a5a8009955";
+
+const Search = ({ foodData, setFoodData }) => {
   const [query, setQuery] = useState("pizza");
   useEffect(() => {
-    function demo() {
-      console.log("Demo function executed");
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data.results);
+      setFoodData(data.results);
     }
-    demo();
+    fetchFood();
   }, [query]);
 
   return (
